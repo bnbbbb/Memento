2d6465206964de092d6b20dcfd6139aee4a30df4
bnbbbb <zio7542@naver.com>
2024-08-03 01:01:04 +0900

    수정사항

--- app/database/connection.py
+++ app/database/connection.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+# .env 파일을 app폴더에 저장할 때
+# BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+# .env 파일을 외부에 저장할 때
+BASE_DIR = Path(__file__).resolve().parent.parent.parent / '.env'
 load_dotenv(dotenv_path=BASE_DIR)
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}"
@@ -14,6 +18,7 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+    print(BASE_DIR)
     try:
         yield db
     finally:
